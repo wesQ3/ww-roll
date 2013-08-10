23ee4fc09f33d1c9b29aa9bee0188fee5dee5733
Wes Malone <wesdmalone@gmail.com>
2013-08-10 13:45:57 -0500

    accumulate mode

--- ww_roll.pl
+++ ww_roll.pl
@@ -7,10 +7,11 @@ use Getopt::Long::Descriptive;
 use Games::Dice::Advanced;
 
 my ($opt, $usage) = describe_options(
-  'send-sms %o <some-arg>',
+  'ww_roll %o <some-arg>',
   [ 'reroll|r=i',  'Minimum to reroll',         { default => 10 }],
   [ 'dice|x=i',    'Amount of dice to roll',    { default => 1 } ],
   [ 'success|s=i', 'Minimum value for success', { default => 8 } ],
+  [ 'accumulate|a=i', 'Count attempts to reach required successes', { default => 0 } ],
   [],
   [ 'verbose|v',   'verbose'                                     ],
   [ 'help|h',     'view help'                                    ],
@@ -18,13 +19,27 @@ my ($opt, $usage) = describe_options(
 
 print($usage->text), exit if $opt->help;
 
-printf "Expected successes: %.2f\n", $opt->dice*(11 - $opt->success)/($opt->reroll - 1);
+if ($opt->accumulate) {
+   say sprintf 'Accumulating %d successes',$opt->accumulate;
+   my ( $super_total, $attempt_count ) = 0;
+   while ($super_total < $opt->accumulate) {
+      $super_total += attempt();
+      $attempt_count++;
+   }
+   say "Succeeded in $attempt_count attempts";
+} else {
+   printf "Expected successes: %.2f\n", $opt->dice*(11 - $opt->success)/($opt->reroll - 1);
+   attempt()
+}
 
-say 'Rolling ' . $opt->dice . ' dice...';
-my @result =  roll($opt->dice);
-say join ' ', sort {$b <=> $a} @result if $opt->verbose;
-my $successes = scalar grep {$_ >= $opt->success} @result;
-say "$successes successes";
+sub attempt {
+   say 'Rolling ' . $opt->dice . ' dice...';
+   my @result =  roll($opt->dice);
+   say join ' ', sort {$b <=> $a} @result if $opt->verbose;
+   my $successes = scalar grep {$_ >= $opt->success} @result;
+   say "$successes successes";
+   return $successes
+}
 
 sub roll {
    state $d10 = Games::Dice::Advanced->new('d10');
